468516298aeb1ae1bcbc3d3ab02a222cc994098c
Enrique1311 <ejspinelli131176@gmail.com>
2024-12-05 00:14:24 -0300

    Classes

---
+++ src/01_class.ts
@@ -0,0 +1,14 @@
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const date1 = new MyDate(2000, 1, 1);
+console.log(date1);
